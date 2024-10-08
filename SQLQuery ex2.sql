1af7c691ed141a1db7adb29b854ac61413989140
Gustavo Koyanagui <160496975+Gutoo07@users.noreply.github.com>
2024-10-08 17:28:41 -0300

    Populando com dados e testando todas Constraints

--- SQLQuery ex2.sql
+++ SQLQuery ex2.sql
@@ -67,3 +67,58 @@ foreign key(editoraId) references editora(id),
 foreign key (edicaoIsbn) references edicao(isbn),
 foreign key (livroCodigo) references livro(codigo)
 )
+----------------------------------------------------
+select * from livro
+select * from edicao
+select * from autor
+select * from editora
+select * from livro_autor
+select * from editora_edicao_livro
+----------------------------------------------------
+insert into livro (nome, lingua, ano) values
+('Duna 1', 'EN-US', 1990)
+insert into livro (nome, lingua, ano) values
+('Duna 2', 'EN-US', 1989)
+insert into livro (nome, ano) values
+('Duna 3', 1993)
+
+insert into autor (nome, data_nasc, pais_nasc, biografia) values
+('Frank', '1960-03-21', 'Estados Unidos', 'Lisan al Gaib')
+insert into autor (nome, data_nasc, pais_nasc, biografia) values
+('Frank', '1972-04-19', 'Estados Unidos', 'Esse aqui nao entra')
+insert into autor (nome, data_nasc, pais_nasc, biografia) values
+('Herbert', '1980-05-12', 'Alemanha', 'Hello World')
+insert into autor (nome, data_nasc, pais_nasc, biografia) values
+('Lucas', '1992-03-17', 'Japao', 'Esse também nao rola')
+
+insert into livro_autor values
+(100001, 2353)
+
+insert into edicao values
+('1234567891234', 1, 1993, 15, 0)
+insert into edicao values --len(isbn) != 13
+('1234', 50, 1994, 500, 10)
+insert into edicao values --preco < 0
+('1234567891235', -5, 1993, 15, 0)
+insert into edicao values --ano < 1993
+('1234567891235', 30, 1992, 15, 0)
+insert into edicao values --numero_paginas < 15
+('1234567891235', 30, 1993, 10, 0)
+insert into edicao values
+('1234567891235', 30, 2015, 470, 1042)
+
+insert into editora (nome, telefone, log_end, num_end, cep_end, comp_end) values
+('Astro', '11912345678', 'Rua Avenida 2', 1560, '01234567', 'Bairro Vila')
+insert into editora (nome, telefone, log_end, num_end, cep_end, comp_end) values --nome duplicado
+('Astro', '11912345678', 'Rua Avenida 2', 1560, '01234567', 'Bairro Vila')
+insert into editora (nome, telefone, log_end, num_end, cep_end, comp_end) values --len(telefone) != 11
+('Cometa', '119123', 'Estrada Incrivel', 12, '23456789', 'Casa')
+insert into editora (nome, telefone, log_end, num_end, cep_end, comp_end) values --num_end < 0
+('Cometa', '11934567890', 'Estrada Incrivel', -14, '01234567', 'Bairro Vila')
+insert into editora (nome, telefone, log_end, num_end, cep_end, comp_end) values --len(cep) != 8
+('Cometa', '11934567890', 'Estrada Incrivel', 15, '01234', 'Bairro Vila')
+insert into editora (nome, telefone, log_end, num_end, cep_end, comp_end) values
+('Omega', '11945450909', 'Av Aguia de Haia', 2633, '03344556', 'Fatec')
+
+insert into editora_edicao_livro values 
+(603, 1234567891235, 100201)
